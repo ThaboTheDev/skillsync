06d4d7353cc3bd8f71464ff5c0e633fc76c22bf4
wtc <wtc@pop-os.localdomain>
2025-01-24 13:31:00 +0200

    updated the UI

--- flutter/skillsync/lib/screens/mentorsScreen.dart
+++ flutter/skillsync/lib/screens/mentorsScreen.dart
@@ -49,12 +49,94 @@ class _MentorsscreenState extends State<Mentorsscreen> {
     );
   }
 
+  Widget _upcomingMeeting() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.greenAccent,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20)
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'Start time:',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    'time',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'End time:',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    'time',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'Title:',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    'title',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           _topPart(),
+          _upcomingMeeting(),
         ],
       ),
     );
