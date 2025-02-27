65149dc2439f094fd45dcff2a667a4304c711a77
thmlilojhb024 <thmlilojhb024@student.wethinkcode.co.za>
2025-02-27 16:46:22 +0200

    started on the profile screen

--- flutter/skillsync/lib/screens/profilescreen.dart
+++ flutter/skillsync/lib/screens/profilescreen.dart
@@ -11,6 +11,55 @@ class _ProfilescreenState extends State<Profilescreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Profile",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Center(
+                  child: IconButton(
+                    onPressed: () {}, 
+                    iconSize: 100,
+                    icon: Icon(Icons.account_circle),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("@FullName"),
+                    Text("email@emailaddress.com"),
+                  ],
+                ),
+              ],
+            ),
+            ElevatedButton.icon(
+              onPressed: () {}, 
+              icon: Icon(Icons.edit), 
+              label: const Text("Edit"),
+            ),
+            const SizedBox(height: 10,),
+            const Text(
+              "Details:",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
